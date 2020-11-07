e9a2fc592097d0a596c0182875094d55e84bcd64
amukancabbage <amukancabbage@gmail.com>
2020-11-07 20:49:07 +0800

    Membuat SplashActivity.kt

--- app/src/main/java/com/mirzayogy/praktikumkelas5/MainActivity.kt
+++ app/src/main/java/com/mirzayogy/praktikumkelas5/MainActivity.kt
@@ -2,10 +2,32 @@ package com.mirzayogy.praktikumkelas5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val etAngkaPertama = findViewById<EditText>(R.id.etAngkaPertama)
+        val etAngkaKedua = findViewById<EditText>(R.id.etAngkaKedua)
+        val btnHitung = findViewById<Button>(R.id.btnHitung)
+        val tvHasil = findViewById<TextView>(R.id.tvHasil)
+
+        btnHitung.setOnClickListener {
+//            Toast.makeText(this,"Tombol diklik",Toast.LENGTH_SHORT).show()
+
+            val angkaPertama = etAngkaPertama.text.toString().toDouble()
+            val angkaKedua = etAngkaKedua.text.toString().toDouble()
+
+            val hasil = angkaPertama + angkaKedua
+            tvHasil.text = hasil.toString()
+        }
+
     }
 }
