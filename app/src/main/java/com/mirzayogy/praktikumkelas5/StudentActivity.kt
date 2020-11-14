2a36212fd9e7d63bca9e303691f881f7b07baa09
amukancabbage <amukancabbage@gmail.com>
2020-11-14 20:32:15 +0800

    Intent DIAL

--- app/src/main/java/com/mirzayogy/praktikumkelas5/StudentActivity.kt
+++ app/src/main/java/com/mirzayogy/praktikumkelas5/StudentActivity.kt
@@ -1,6 +1,7 @@
 package com.mirzayogy.praktikumkelas5
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -12,9 +13,11 @@ class StudentActivity : AppCompatActivity() {
         setContentView(R.layout.activity_student)
 
         val btSendStudent = findViewById<Button>(R.id.btSendStudent)
+        val btDialStudent = findViewById<Button>(R.id.btDialStudent)
         val etIdStudent = findViewById<EditText>(R.id.etIdStudent)
         val etNameStudent = findViewById<EditText>(R.id.etNameStudent)
         val etPhoneNumberStudent = findViewById<EditText>(R.id.etPhoneNumberStudent)
+
 
         btSendStudent.setOnClickListener {
 
@@ -22,10 +25,17 @@ class StudentActivity : AppCompatActivity() {
             val name = etNameStudent.text.toString()
             val phoneNumber = etPhoneNumberStudent.text.toString()
 
+
             val i = Intent(this,ResultActivity::class.java)
             i.putExtra(ResultActivity.EXTRA_ID,id)
             i.putExtra(ResultActivity.EXTRA_NAME,name)
             i.putExtra(ResultActivity.EXTRA_PHONE_NUMBER,phoneNumber)
+            startActivity(i)
+        }
+
+        btDialStudent.setOnClickListener {
+            val phoneNumber = etPhoneNumberStudent.text.toString()
+            val i = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
             startActivity(i)
         }
     }
